daae274b6348f2af0abc0c0bbc6131575f602316
RISHABH KUMAR PANDEY <rkp98352@gmail.com>
2025-06-09 17:18:21 +0530

    .env moved out of git ignore

--- server/models/Room.js
+++ server/models/Room.js
@@ -2,5 +2,13 @@
 const mongoose = require("mongoose")
 
 const RoomSchema = new mongoose.Schema({
-    
+    // it will have username,  start time , is_open, cool_down_time, meet id and password
+    name : String,
+    start_time : Date,
+    is_open : Boolean,
+    cool_down_time : Number,
+    meetid : String,
+    password : String,
+
 })
+module.exports = mongoose.model('Room', RoomSchema);
